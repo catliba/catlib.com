e8a64bab0ae16501a59ba1332459ac8cb95e844b
Caleb Li <1caleblili@gmail.com>
2023-07-08 00:38:25 -0700

    finished logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,38 @@ import { useState } from 'react'
 import React from 'react'
 import PageFlip from './components/page-flip'
 import './css/App.css'
+import LogoClick from './pngs/Logo_Click.png'
+import LogoHover from './pngs/Logo_Hover.png'
+import Logo from './pngs/Logo_Start.png'
 
 // This page will feature the page flip introduction animation
 
 function App() {
+  const [logoImage, setLogoImage] = useState(Logo);
+
+  const handleHover = () => {
+    setLogoImage(LogoHover);
+  }
+  const handleClick = () => {
+    setLogoImage(LogoClick);
+  }
   return (
     <>
-      <p> Hi </p>
+      <img 
+        className="logo" 
+        src={logoImage}
+        useMap='#logo-map' 
+        alt="Catlib.com" 
+      />
+      <map name="logo-map">
+        <area 
+          shape='poly' 
+          coords="153,499,147,578,157,637,181,683,212,721,252,757,293,775,331,792,377,801,430,808,485,811,542,811,613,804,997,761,1262,735,1333,717,1377,704,1414,682,1446,646,1470,598,1482,550,1482,510,1475,466,1449,400,1410,354,1368,325,1333,295,1284,270,1253,260,1171,60,1120,217,907,238,786,251,383,291,249,173,238,332,194,395,172,422,160,451"
+          onMouseOver={handleHover}
+          onMouseLeave={() => setLogoImage(Logo)}
+          onClick={handleClick}
+        />
+      </map>
     </>
   )
 }
