d3dc253362d7638907737dca6392cf28f88e04ef
Caleb <1caleblili@gmail.com>
2023-07-09 13:32:39 -0700

    about me page layout

--- src/App.tsx
+++ src/App.tsx
@@ -18,23 +18,27 @@ function App() {
     setLogoImage(LogoClick);
   }
   return (
-    <>
-      <img 
-        className="logo" 
-        src={logoImage}
-        useMap='#logo-map' 
-        alt="Catlib.com" 
-      />
-      <map name="logo-map">
-        <area 
-          shape='poly' 
-          coords="153,499,147,578,157,637,181,683,212,721,252,757,293,775,331,792,377,801,430,808,485,811,542,811,613,804,997,761,1262,735,1333,717,1377,704,1414,682,1446,646,1470,598,1482,550,1482,510,1475,466,1449,400,1410,354,1368,325,1333,295,1284,270,1253,260,1171,60,1120,217,907,238,786,251,383,291,249,173,238,332,194,395,172,422,160,451"
-          onMouseOver={handleHover}
-          onMouseLeave={() => setLogoImage(Logo)}
-          onClick={handleClick}
+    <div className='app'>
+      <div className='logo'>
+        <img 
+          src={logoImage}
+          useMap='#logo-map' 
+          alt="Catlib.com" 
         />
-      </map>
-    </>
+        <map name="logo-map">
+          <area 
+            shape='poly' 
+            coords="153,499,147,578,157,637,181,683,212,721,252,757,293,775,331,792,377,801,430,808,485,811,542,811,613,804,997,761,1262,735,1333,717,1377,704,1414,682,1446,646,1470,598,1482,550,1482,510,1475,466,1449,400,1410,354,1368,325,1333,295,1284,270,1253,260,1171,60,1120,217,907,238,786,251,383,291,249,173,238,332,194,395,172,422,160,451"
+            onMouseOver={handleHover}
+            onMouseLeave={() => setLogoImage(Logo)}
+            onClick={handleClick}
+          />
+        </map>
+      </div>
+      <div className='logo-caption'>
+        <p >My very random website for my very random thoughts</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PageFlip from './page-flip';
 import '../css/about-me.css';
 
 export default function AboutMe() {
+    const containerRef = useRef(null);
+    const [ isVisible, setIsVisible ] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                console.log(entry);
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        });
+        const hiddenElements = document.querySelectorAll('.hidden');
+        hiddenElements.forEach((el) => observer.observe(el));
+    }, )
+
     return (
         <>
-            <div className='about-me'>
-            Hi. Before you look around my site, I would like to tell you a little bit about myself. 
-            My name is Caleb but some of my friends call me catlib (it's a long story). I like cats.
+            <div className='content'>
+                <div className='hidden about-me'>
+                    <p> 
+                        <span>Hi</span>My name is Caleb but some of my friends call me catlib.
+                    </p>
+                </div>
+                <div className='hidden languages'>
+                    <h1>My Technical Background</h1>
+                    <dl>
+                        <dt>HTML,CSS,JavaScript (add image later)</dt>
+                        <dd>How else would I make this website?</dd>
+
+                        <dt>Java</dt>
+                        <dd>My first language</dd>
+
+                        <dt>C</dt>
+                        <dd>Talk about what I did in CSE30</dd>
+
+                        <dt>React</dt>
+                        <dd>Who doesn't love React?</dd>
+
+                        <dt>Node.js (MongoDB)</dt>
+                        <dd>Backend for projects: (link project)</dd>
+
+                        <dt>Docker</dt>
+                    </dl>
+                    <h1>What I'm Interested In</h1>
+                    <p></p>
+                </div>
+                <div className='hidden achievements'>
+                    <h1>Accomplishments</h1>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+                <div className='hidden watching'>
+                    <div className='shows'>
+                        <h1>Shows:</h1>
+                    </div>
+                    <div className='books'>
+                        <h1>Books:</h1>
+                    </div>
+                </div>
+                <div className='hidden contacts'>
+                    <h1>Contacts:</h1>
+                    <ul>
+                        <li>Email</li>
+                        <li>LinkedIn</li>
+                        <li>Insta</li>
+                        <li>Snap</li>
+                        <li>Spotify</li>
+                        <li>Discord</li>
+                        <li>Facebook</li>
+                        <li>BeReal</li>
+                        <li>Chess</li>
+                    </ul>
+                </div>
             </div>
             <PageFlip />
         </>
